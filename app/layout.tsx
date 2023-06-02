82ddccba5584c5869bc419db0bfeec59c2375593
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2023-06-02 18:09:49 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import '#/styles/globals.css';
 import { AddressBar } from '#/ui/address-bar';
-import Byline from '#/ui/byline';
 import { GlobalNav } from '#/ui/global-nav';
 import { Metadata } from 'next';
 
@@ -24,17 +23,16 @@ export default function RootLayout({
         <GlobalNav />
 
         <div className="lg:pl-72">
-          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
+            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black">
                 <AddressBar />
               </div>
             </div>
 
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
             </div>
-            <Byline className="fixed sm:hidden" />
           </div>
         </div>
       </body>
--- ui/global-nav.tsx
+++ ui/global-nav.tsx
@@ -7,7 +7,6 @@ import { useSelectedLayoutSegment } from 'next/navigation';
 import { MenuAlt2Icon, XIcon } from '@heroicons/react/solid';
 import clsx from 'clsx';
 import { useState } from 'react';
-import Byline from './byline';
 
 export function GlobalNav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +14,7 @@ export function GlobalNav() {
 
   return (
     <div className="fixed top-0 z-10 flex w-full flex-col border-b border-gray-800 bg-black lg:bottom-0 lg:z-auto lg:w-72 lg:border-b-0 lg:border-r lg:border-gray-800">
-      <div className="flex h-14 items-center py-4 px-4 lg:h-auto">
+      <div className="flex h-14 items-center px-4 py-4 lg:h-auto">
         <Link
           href="/"
           className="group flex w-full items-center gap-x-2.5"
@@ -68,7 +67,6 @@ export function GlobalNav() {
             );
           })}
         </nav>
-        <Byline className="absolute hidden sm:block" />
       </div>
     </div>
   );
