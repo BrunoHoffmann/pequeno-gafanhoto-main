db4423fc34cd578e32c36eadb3664260c175fffb
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2023-06-02 17:48:46 +0100

    update files conflit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,32 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="/pg-logo.png" rel="apple-touch-icon" sizes="180x180" />
-        <link href="/pg-logo.png" rel="icon" sizes="32x32" type="image/png" />
-        <link href="/pg-logo.png" rel="icon" sizes="16x16" type="image/png" />
+        <link
+          href="/favicon/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicon/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicon/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
         <link href="/favicon/site.webmanifest" rel="manifest" />
-        <link color="#000000" href="/pg-logo.svg" rel="mask-icon" />
+        <link
+          color="#000000"
+          href="/favicon/safari-pinned-tab.svg"
+          rel="mask-icon"
+        />
         <link href="/favicon/favicon.ico" rel="shortcut icon" />
       </Head>
-      <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')]">
+      <body className="overflow-y-scroll bg-gray-1100 bg-[url('/grid.svg')]">
         <Main />
         <NextScript />
       </body>
